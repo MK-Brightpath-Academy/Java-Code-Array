81119c746f5cafbb55be1c799b6402c89567f974
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2025-10-22 16:10:21 +0530

    Time: 2 ms (34.37%) | Memory: 44.5 MB (82.77%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+         List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList)); // Add a copy of current permutation
+        } else {
+            for (int num : nums) {
+                if (tempList.contains(num)) continue; // Skip used numbers
+                tempList.add(num);
+                backtrack(result, tempList, nums);
+                tempList.remove(tempList.size() - 1); // Backtrack
+            }
+    }}
+}
